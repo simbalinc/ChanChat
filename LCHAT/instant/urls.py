ae216e959a1b2c9d7e123ac079fbffc40bfce5d3
lincmba <lincolncmba@gmail.com>
2017-11-19 20:42:27 +0300

    Authentication  login  sign up  logout

--- LCHAT/instant/urls.py
+++ LCHAT/instant/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
-from .views import user_list
+from .views import user_list, log_in, log_out, sign_up
 
 urlpatterns = [
+    url(r'^log_in/$', log_in, name='log_in'),
+    url(r'^log_out/$', log_out, name='log_out'),
+    url(r'^sign_up/$', sign_up, name='sign_up'),
     url(r'^$', user_list, name='user_list'),
     ]
--- LCHAT/instant/views.py
+++ LCHAT/instant/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.urlresolvers import reverse
 
 
 def user_list(request):
     return render(request, 'instant/user_list.html')
+
+
+def log_in(request):
+    """
+    Form checks if there's a supplied username and password then returns the
+    user object.
+    We then log in the authenticated user and redirect them to the homepage
+    """
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(reverse('instant:user_list'))
+        else:
+            print(form.errors)
+    return render(request, 'instant/log_in.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('instant:log_in')
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('instant:log_in'))
+        else:
+            print(form.errors)
+    return render(request, 'instant/sign_up.html', {'form': form})
+
+
