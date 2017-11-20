80c1c4e6443b48814d83c719530c000cd94f8ec9
lincmba <lincolncmba@gmail.com>
2017-11-20 22:05:40 +0300

    Login Alerts  Models  Signals

--- LCHAT/instant/consumers.py
+++ LCHAT/instant/consumers.py
@@ -5,12 +5,28 @@ Are a counterpart of Django views
 Any user connected to our server will be added to the users group and receive
 messages from the server
 """
+import json
 from channels import Group
+from channels.auth import channel_session_user, channel_session_user_from_http
 
 
+@channel_session_user_from_http
 def ws_connect(message):
     Group('users').add(message.reply_channel)
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': True,
+            })
+        })
 
 
+@channel_session_user
 def ws_disconnect(message):
+    Group('users').send({
+        'text': json.dumps({
+            'username': message.user.username,
+            'is_logged_in': False,
+            })
+        })
     Group('users').discard(message.reply_channel)
